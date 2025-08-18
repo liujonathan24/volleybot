2f5a62ee4540af50ea6f759bfcad4927d781e753
Jonathan Liu <liujonathan24@gmail.com>
2025-08-17 22:17:59 -0400

    update

--- agents/train_agent.py
+++ agents/train_agent.py
@@ -6,7 +6,7 @@ def main(args):
     result = subprocess.run(['ls', '-l'], capture_output=True, text=True, check=True)
 
     # Execute a command without capturing output
-    subprocess.run(['mkdir', 'new_directory'])
+    subprocess.run(['mkdir', f'logs/{args.run_name}'])
 
 
 if __name__ == "__main__":
@@ -15,6 +15,8 @@ if __name__ == "__main__":
     parser.add_argument("--model", "-m", default="PPO", type=str, help="Current implemented algorithms is: PPO")
     parser.add_argument("--steps", "-t", default=100, type=int)
     parser.add_argument("--batch_size", "-b", default=1, type=int)
+    parser.add_argument("--batch_size", "-b", default=1, type=int)
+    parser.add_argument("--run_name", "-n", default="", type=str)
 
     args = parser.parse_args()
     main(args)
